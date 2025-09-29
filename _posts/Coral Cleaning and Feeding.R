32bb959c481e23b1c8abf81dac3ac79981e56e48
jessica-mongeau <jessica_mongeau@uri.edu>
2025-09-29 11:59:56 -0400

    Create Coral Cleaning and Feeding.R

---
+++ _posts/Coral Cleaning and Feeding.R
@@ -0,0 +1,50 @@
+layout: post
+title: Protocol for Feeding Corals and Cleaning Coral Plugs and Side Walls
+date: '2025-9-29'
+categories: Protocols
+tags: coral, CBLS_aquarium
+projects: CBLS, Putnam Lab
+---
+  
+  # Protocol procedure for feeding corals and cleaning coral plugs and side walls
+  
+  ## Feeding
+  Corals must be fed once a week. 
+  
+  ### Materials
+- Corals in crates
+- 4 Feeding Bins
+- Coral Max (Coral Food)
+- 1 tsp scoop
+- 2 cup measuring cup 
+
+  ### How To
+1. Turn off the pumps to the system. Remove one half of the tank lids. 
+2. Place 4 feeding bins on top of the tank lids that were not removed.
+3. Pour 4 cups of water into each feeding bin. Make sure water does not get in between the cracks of the system. 
+4. Take 1 tsp of coral food (Coral Max) and swirl into the feeding bin. 
+5. Carefully place coral crates into feeding bins. Move corals into other feeding bins if there is not enough room for all of them to fit. 
+6. Ensure each coral is submerged. Add more water if needed. 
+7. Turn back on the pumps to the system.
+8. Let corals sit for 10 minutes. 
+9. After 10 minutes, turn the pumps back off. 
+10. Remove the coral crates from the feeding bins and place them back into the system. Replace any corals that were moved back into the appropriate crate. 
+11. Reswirl the food in the feeding bin.
+12. Repeat steps 5 through 10. Corals can be fed with the same food for 2 rounds of feeding.
+13. After 2 rounds of feeding, use the measuring cup and remove some water from the feeding bins. Pour down the floor drain. 
+14. Pour the rest of the water out of the feeding bins down the floor drain.
+15. Repeat this process for the rest of the corals in Blue Tank 1/2 and Blue Tank 3/4. 
+
+## Cleaning Coral Plugs
+### Materials
+- Coral crates in system
+- Coral crate replacement (should be the same number as the one you are cleaning in the tank)
+- Toothbrush
+- Bleach Bucket
+- Tweezers
+
+### How To
+1. Remove the tank lid from the tank and place your tools in a reachable secure area.
+2. Remove the coral plug from the crate, holding it by the base or sides of the plug. Try to not touch the coral itself. 
+3. Remove algea on plugs by gently brushing over it with a toothbrush. Apply more pressure on the plug itself, but be gentle when removing algae from the corals. 
+4. In cases where filamentous algea is found on coral plugs that cannot be removed with toothbrush, remove it by gently plucking it off with tweezers. 
